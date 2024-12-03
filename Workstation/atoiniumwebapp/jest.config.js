4b1fd0e9add221379479115fd038a2a523d36149
guxegdsa <ofidohub@gmail.com>
2024-12-03 19:10:26 +0200

    commit "thera application setup"

--- Workstation/atoiniumwebapp/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
-    testEnvironment: 'jsdom',
-    moduleNameMapper: {
-      '\\.(css|less)$': 'identity-obj-proxy'
-    }
-  };
